1d93682377325b0b112cfd101523bab366062875
San_win <San_win@testsyncobs.com>
2022-12-20 11:05:42 +0100

    prise en main de VueJS + test de connexion axios

--- tuto_vuejsdb/ajaxrequetes.php
+++ tuto_vuejsdb/ajaxrequetes.php
@@ -1,43 +1,43 @@
 <?php
-echo json_encode("[{nom : arnu}]")
-// // On inclut la connexion à la base de données pour pouvoir envoyer les requêtes et récupérer les données
-// include 'config.php';
+// echo json_encode("[{nom : arnu}]")
+// On inclut la connexion à la base de données pour pouvoir envoyer les requêtes et récupérer les données
+include 'config.php';
 
-// // On récupère les données envoyées par la requête AJAX
+// On récupère les données envoyées par la requête AJAX
 
-// $condition = '1';
-// // On vérifie si l'ID est envoyé, si c'est le cas, on associe la valeur de condition à l'ID, suivi par la valeur de l'ID (id = 1, id = 2, etc. qui correspond à l'ID de l'utilisateur)
-// if (isset($_GET['id'])) {
-//     $condition = 'id = ' . $_GET['idUtilisateur'];
-// }
-
-
-// $donneesUser = mysqli_query($db, 'SELECT * FROM utilisateurs WHERE ' . $condition);
-// $reqQuery = array(); // On crée un tableau vide pour stocker les données
-
-// // On parcourt les données récupérées et on initialise le tableau avec les données récupérées
-
-// while($donnees = mysqli_fetch_assoc($donneesUser)) {
-//     $reqQuery[] = $donnees; // On ajoute les données dans le tableau
-// }
+$condition = '1';
+// On vérifie si l'ID est envoyé, si c'est le cas, on associe la valeur de condition à l'ID, suivi par la valeur de l'ID (id = 1, id = 2, etc. qui correspond à l'ID de l'utilisateur)
+if (isset($_GET['id'])) {
+    $condition = 'id = ' . $_GET['idUtilisateur'];
+}
 
 
-// echo json_encode($reqQuery); // On envoie en JSON car le résultat retourné est un tableau associatif
+$donneesUser = mysqli_query($connexion, 'SELECT * FROM utilisateurs WHERE '.$condition);
+$reqQuery = array(); // On crée un tableau vide pour stocker les données
 
-// // exemple :
+// On parcourt les données récupérées et on initialise le tableau avec les données récupérées
 
-// // $reqQuery = array(
-// //     0 => array(
-// //         'id' => 1,
-// //         'username' => 'lucas1',
-// //         'nom' => 'arnu',
-// //         'email' => 'lucas.arnu@gmail.com'
-// //     ),
-// //     1 => array(
-// //         'id' => 2,
-// //          'username' => 'matteo2',
-// //          etc... 
+while($donnees = mysqli_fetch_assoc($donneesUser)) {
+    $reqQuery[] = $donnees; // On ajoute les données dans le tableau
+}
 
 
-// exit; 
-// ?>
+echo json_encode($reqQuery); // On envoie en JSON car le résultat retourné est un tableau associatif
+
+// exemple :
+
+// $reqQuery = array(
+//     0 => array(
+//         'id' => 1,
+//         'username' => 'lucas1',
+//         'nom' => 'arnu',
+//         'email' => 'lucas.arnu@gmail.com'
+//     ),
+//     1 => array(
+//         'id' => 2,
+//          'username' => 'matteo2',
+//          etc... 
+
+
+exit; 
+?>
--- tuto_vuejsdb/config.php
+++ tuto_vuejsdb/config.php
@@ -1,12 +1,24 @@
 <?php
 
+// $host = "localhost"; // là où se trouve l'app phpmyadmin
+// $username = "projetsae";
+// $password = "";
+// $dbname = "";
+
+// $connexion = mysqli_connect($host, $username, $password, $dbname);
+
+// if (!$connexion) {
+//     die("Connexion échouée : " . mysqli_connect_error());
+// }
+
+// Connexion avec le serveur WAMP (Matteo)
+
 $host = "localhost"; // là où se trouve l'app phpmyadmin
-$username = "projetsae";
+$username = "root";
 $password = "";
-$dbname = "";
+$dbname = "sae301";
 
 $connexion = mysqli_connect($host, $username, $password, $dbname);
-
 if (!$connexion) {
     die("Connexion échouée : " . mysqli_connect_error());
 }
