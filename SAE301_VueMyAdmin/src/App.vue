bc2854ce40c2eb0ae4132de12da5b195f0ede614
soc1ety <70237736+soc1ety@users.noreply.github.com>
2022-12-20 12:48:01 +0100

    importation du component showdbtables

--- SAE301_VueMyAdmin/src/App.vue
+++ SAE301_VueMyAdmin/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ShowDBTables />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShowDBTables from './components/ShowDBTables.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShowDBTables
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
