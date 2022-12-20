e6d8a7fa499b31252722aaa7e7789f0914178484
soc1ety <70237736+soc1ety@users.noreply.github.com>
2022-12-20 12:47:22 +0100

    affichage des bases de données d'un server

---
+++ SAE301_VueMyAdmin/src/components/ShowDBTables.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>Bases de données présentes sur le server localhost</h1>
+      <ul>
+        <li v-for="database in databases" :key="database">{{ database }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        databases: []
+      }
+    },
+    mounted() {
+      axios.get('http://localhost/db.php')
+        .then(response => {
+          this.databases = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+  </script>
+ 
