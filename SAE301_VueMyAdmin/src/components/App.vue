8726c4da104cb43e396dbe5423de861497c37c25
soc1ety <70237736+soc1ety@users.noreply.github.com>
2022-12-28 16:00:39 +0100

    refactoring des composants

---
+++ SAE301_VueMyAdmin/src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <panel-view></panel-view>
+  </div>
+</template>
+
+<script>
+import PanelView from './PanelView.vue';
+
+export default {
+  components: {
+    PanelView,
+  },
+};
+</script>
