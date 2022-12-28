762d8b8fbe2d328b28f0714014616eed77b6f829
soc1ety <70237736+soc1ety@users.noreply.github.com>
2022-12-28 15:57:48 +0100

    refactoring des composants

--- SAE301_VueMyAdmin/src/components/ShowDatabases.vue
+++ SAE301_VueMyAdmin/src/components/ShowDatabases.vue
@@ -1,20 +1,86 @@
 <template>
-  <div>
-    <ShowTables :database="selectedDatabase"/>
+  <div class="side-panel">
+    <h3>Liste des bases de données</h3>
+    <ul>
+      <li v-for="database in databases" :key="database.Database" v-on:click="showTables(database.Database)">
+        {{ database.Database }}
+      </li>
+    </ul>
+    <h3>Table(s)</h3>
+    <ul>
+      <li v-for="table in tables" :key="table.Name">
+        {{ table.Name }}
+        <!-- {{ console.log(table.Name)}} -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import ShowTables from './ShowTables.vue'
+import axios from 'axios';
 
 export default {
-  components: {
-    ShowTables
-  },
   data() {
     return {
-      selectedDatabase: 'sae301_evaluation'
-    }
-  }
-}
+      databases: [],
+      tables: [],
+    };
+  },
+  methods: {
+    showTables(databaseName) {
+      axios.get(`http://100.74.7.84/db.php?database=${databaseName}`)
+        .then(response => {
+         // console.log(response.data);
+          this.tables = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    axios.get('http://100.74.7.84/db.php')
+      .then(response => {
+        this.databases = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+};
 </script>
+
+<style>
+.side-panel {
+  width: 200px;
+  background-color: #f5f5f5;
+  border-right: 1px solid #ddd;
+  height: 100%;
+  overflow-y: auto;
+}
+
+h3 {
+  padding: 10px;
+  margin: 0;
+  font-size: 14px;
+  font-weight: bold;
+  background-color: #eee;
+  border-bottom: 1px solid #ddd;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  margin: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
