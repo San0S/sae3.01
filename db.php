28e77bab0066bd2dbbf56f8137f456e9eec3df89
San_win <San_win@testsyncobs.com>
2023-01-06 13:48:57 +0100

    ajout db.php

---
+++ db.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$host = 'localhost';
+$username = 'root';         // projetsae
+$password = '';             // saebut2projet!
+$dbname = 'SAE301_EVALUATION';
+
+try {
+          $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8'));
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+} catch (PDOException $e) {
+          echo 'Connection failed: ' . $e->getMessage();
+            exit;
+}
+
+if (isset($_GET['database']) && isset($_GET['table'])) {
+          $database = $_GET['database'];
+            $table = $_GET['table'];
+            $query = "DESCRIBE $database.$table";
+            // $query = "SELECT * FROM $database.$table";
+              $stmt = $conn->prepare($query);
+              $stmt->execute();
+                $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                header('Content-Type: application/json');
+                  echo json_encode($data);
+} else if (isset($_GET['database'])) {
+          $database = $_GET['database'];
+            $query = "SHOW TABLES IN $database";
+            $stmt = $conn->prepare($query);
+              $stmt->execute();
+              $tables = $stmt->fetchAll(PDO::FETCH_NUM);
+
+                header('Content-Type: application/json');
+                echo json_encode($tables);
+} else {
+          $query = 'SHOW DATABASES';
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+              $databases = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+              header('Content-Type: application/json');
+                echo json_encode($databases);
+}
