a9aa021d90d6f3a889dc951f646f8148dd53a904
San_win <San_win@testsyncobs.com>
2023-01-08 03:09:01 +0100

    maj db.php pour l'affichage des structures

--- db.php
+++ db.php
@@ -2,43 +2,60 @@
 header("Access-Control-Allow-Origin: *");
 
 $host = 'localhost';
-$username = 'root';
-$password = '';
+$username = 'root';         // projetsae
+$password = '';             // saebut2projet!
 $dbname = 'SAE301_EVALUATION';
 
 try {
-          $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8'));
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+
 } catch (PDOException $e) {
-          echo 'Connection failed: ' . $e->getMessage();
-            exit;
+  echo 'Connection failed: ' . $e->getMessage();
+  exit;
 }
 
-if (isset($_GET['database']) && isset($_GET['table'])) {
-          $database = $_GET['database'];
-            $table = $_GET['table'];
-            $query = "SELECT * FROM $database.$table";
-              $stmt = $conn->prepare($query);
-              $stmt->execute();
-                $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_GET['database']) && isset($_GET['table']) && isset($_GET['structure'])) {
+  $database = $_GET['database'];
+  $table = $_GET['table'];
+  $query = "DESCRIBE $database.$table";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  $structure = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                header('Content-Type: application/json');
-                  echo json_encode($data);
+  header('Content-Type: application/json');
+  echo json_encode($structure);
+
+
+} else if (isset($_GET['database']) && isset($_GET['table'])) {
+  $database = $_GET['database'];
+  $table = $_GET['table'];
+  $query = "SELECT * FROM $database.$table";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  header('Content-Type: application/json');
+  echo json_encode($data);
+
+
 } else if (isset($_GET['database'])) {
-          $database = $_GET['database'];
-            $query = "SHOW TABLES IN $database";
-            $stmt = $conn->prepare($query);
-              $stmt->execute();
-              $tables = $stmt->fetchAll(PDO::FETCH_NUM);
+  $database = $_GET['database'];
+  $query = "SHOW TABLES IN $database";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  $tables = $stmt->fetchAll(PDO::FETCH_NUM);
 
-                header('Content-Type: application/json');
-                echo json_encode($tables);
+  header('Content-Type: application/json');
+  echo json_encode($tables);
+
+
 } else {
-          $query = 'SHOW DATABASES';
-            $stmt = $conn->prepare($query);
-            $stmt->execute();
-              $databases = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $query = 'SHOW DATABASES';
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+  $databases = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-              header('Content-Type: application/json');
-                echo json_encode($databases);
+  header('Content-Type: application/json');
+  echo json_encode($databases);
 }
