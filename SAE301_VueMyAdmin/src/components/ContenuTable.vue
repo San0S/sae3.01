ab6a63b17fa474c1ae08faf7da61525a050693f5
society <root.kali@tutanota.com>
2023-01-10 20:33:03 +0100

    ajout de style pour l'affichage du tableau

--- SAE301_VueMyAdmin/src/components/ContenuTable.vue
+++ SAE301_VueMyAdmin/src/components/ContenuTable.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div>
       <table>
         <thead>
@@ -19,5 +19,47 @@
   export default {
     props: ['columns', 'rows'],
   };
-  </script>
-  
+  </script> -->
+
+
+  <template>
+    <div>
+      <table class="ant-table">
+        <thead>
+          <tr>
+            <th v-for="column in columns" :key="column" class="ant-table-thead-th">{{ column }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in rows" :key="row.id">
+            <td v-for="cell in row" :key="cell" class="ant-table-tbody-td">{{ cell }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+
+<script>
+export default {
+  props: ['columns', 'rows'],
+}
+</script>
+
+<style>
+.ant-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.ant-table-thead-th {
+  font-weight: bold;
+  background-color: #fafafa;
+  text-align: left;
+  padding: 8px;
+}
+
+.ant-table-tbody-td {
+  border: 1px solid #e8e8e8;
+  padding: 8px;
+}
+</style>
