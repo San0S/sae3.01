0cb72451208e4776b665225208a932dc5889b76f
society <root.kali@tutanota.com>
2023-01-12 20:26:00 +0100

    Ajout d'une scroll bar sur l'affichage des tables, se déclenche seulement si la table a un nb de ligne > 10

--- SAE301_VueMyAdmin/src/components/ContenuTable.vue
+++ SAE301_VueMyAdmin/src/components/ContenuTable.vue
@@ -1,65 +1,71 @@
-<!-- <template>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th v-for="column in columns" :key="column">{{ column }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="row in rows" :key="row.id">
-            <td v-for="cell in row" :key="cell">{{ cell }}</td>
-          </tr>
-        </tbody>
-      </table>
+<template>
+  <div>
+    <div class="container" :class="container">
+    <table class="ant-table">
+      <thead>
+        <tr>
+          <th v-for="column in columns" :key="column" class="ant-table-thead-th">{{ column }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in rows" :key="row.id">
+          <td v-for="cell in row" :key="cell" class="ant-table-tbody-td">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ['columns', 'rows'],
-  };
-  </script> -->
-
-
-  <template>
-    <div>
-      <table class="ant-table">
-        <thead>
-          <tr>
-            <th v-for="column in columns" :key="column" class="ant-table-thead-th">{{ column }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="row in rows" :key="row.id">
-            <td v-for="cell in row" :key="cell" class="ant-table-tbody-td">{{ cell }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
+  </div>
+</template>
 
 <script>
 export default {
-  props: ['columns', 'rows'],
+props: ['columns', 'rows'],
+
+data: () => ({
+  // rows: [],
+  nbOfLinesToDisplay: 10
+}),
+
+computed: {
+  container() {
+    if (this.rows.length > this.nbOfLinesToDisplay) {
+      return 'scrollbar-enabled';
+    } else {
+      return 'scrollbar-disabled';
+    }
+  }
 }
+}
+
 </script>
 
 <style>
+
+.scrollbar-enabled {
+  height: 250px;
+  overflow-y: scroll;
+}
+
+.scrollbar-disabled {
+  overflow-y: visible;
+}
+
 .ant-table {
-  width: 100%;
-  border-collapse: collapse;
+width: 100%;
+height: 40px;
+border-collapse: collapse;
+overflow: scroll;
 }
 
 .ant-table-thead-th {
-  font-weight: bold;
-  background-color: #fafafa;
-  text-align: left;
-  padding: 8px;
+font-weight: bold;
+background-color: #fafafa;
+text-align: left;
+padding: 8px;
 }
 
 .ant-table-tbody-td {
-  border: 1px solid #e8e8e8;
-  padding: 8px;
+border: 1px solid #e8e8e8;
+padding: 8px;
 }
 </style>
